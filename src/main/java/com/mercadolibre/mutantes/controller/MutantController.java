a96acbcf02b3dd31dc9a0758584215d9e503fdde
jhonatan.realpe <jhonatan.realpe@pragma.com.co>
2022-04-20 07:47:08 -0500

    Ajuste de controller RequestMutant

--- src/main/java/com/mercadolibre/mutantes/controller/MutantController.java
+++ src/main/java/com/mercadolibre/mutantes/controller/MutantController.java
@@ -1,19 +1,18 @@
 package com.mercadolibre.mutantes.controller;
 
+import com.mercadolibre.mutantes.models.RequestMutant;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.mercadolibre.mutantes.models.ResponseMutant;
 import com.mercadolibre.mutantes.models.ResponseStats;
 import com.mercadolibre.mutantes.service.MutantService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("")
@@ -25,8 +24,8 @@ public class MutantController {
 	private ResponseStats responseStats;
 
 	@PostMapping("/mutant")
-	public ResponseEntity<?> mutant(@RequestParam String[] adn) {
-		responseMutant = mutantService.isMutant(adn);
+	public ResponseEntity<?> mutant(@Valid @RequestBody RequestMutant adn) {
+		responseMutant = mutantService.isMutant(adn.getAdn());
 		JSONObject resp = new JSONObject();
 		resp.put("mensaje", responseMutant.getMensaje());
 		
---
+++ src/main/java/com/mercadolibre/mutantes/models/RequestMutant.java
@@ -0,0 +1,14 @@
+package com.mercadolibre.mutantes.models;
+
+public class RequestMutant {
+
+    String[] adn;
+
+    public String[] getAdn() {
+        return adn;
+    }
+
+    public void setAdn(String[] adn) {
+        this.adn = adn;
+    }
+}
